9a5ca409ebe52a3197c3a6d35c2c2003dfe92c4c
Timothy <yexiaozhou2003@gmail.com>
2022-06-13 23:19:33 +0800

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use actix_web::{web, HttpServer, App, HttpRequest, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-async fn index(req: HttpRequest) -> impl Responder{
+async fn index(req: HttpRequest) -> impl Responder {
     if let Some(ip) = req.connection_info().realip_remote_addr() {
         let v: Vec<&str> = ip.split(":").collect();
         return v[0].to_string();
@@ -13,13 +13,10 @@ async fn index(req: HttpRequest) -> impl Responder{
 async fn main() -> std::io::Result<()> {
     let addr = "0.0.0.0";
     let port = 8000;
-    println!("Server listening at: {}{}", addr, port);
+    println!("Server listening at: {}:{}", addr, port);
 
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-    })
-    .bind((addr, port))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind((addr, port))?
+        .run()
+        .await
 }
